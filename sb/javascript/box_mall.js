efacea3d15f771ac5b6b3e6b3ed76dde832abbde
GamerLu <gamerlu.tw@gmail.com>
2017-06-29 14:42:03 +0800

    2017.06.29

--- sb/javascript/box_mall.js
+++ sb/javascript/box_mall.js
@@ -1,1 +1,1 @@
-hash_box_mall={150:{id:150,name:3328,MB_price:4,launch:1,shelves:2017062911,limit:1,currency_1:1,items:150,str_id:2328,pic:"ads_428.jpg"}};
+hash_box_mall={151:{id:151,name:3327,MB_price:4,launch:1,shelves:2017070611,limit:1,currency_1:1,items:151,str_id:2327,pic:"ads_431.jpg"}};
