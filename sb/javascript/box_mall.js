1312b4f46e2f2ddaad022a0a81e5e4a2ae20f08d
GamerLu <gamerlu.tw@gmail.com>
2017-10-05 16:08:57 +0800

    2017.10.05

--- sb/javascript/box_mall.js
+++ sb/javascript/box_mall.js
@@ -1,1 +1,1 @@
-hash_box_mall={164:{id:164,name:3307,MB_price:4,launch:1,shelves:2017100511,limit:1,currency_1:1,items:164,str_id:2307,pic:"ads_307.jpg"}};
+hash_box_mall={165:{id:165,name:3309,MB_price:4,launch:1,shelves:2017101211,limit:1,currency_1:1,items:165,str_id:2309,pic:"ads_309.jpg"}};
