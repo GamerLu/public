fd4938a23bca1109d2ba7b5a2a894e49ed66161d
GamerLu <gamerlu.tw@gmail.com>
2018-01-22 11:38:27 +0800

    2018.01.18

--- sb/javascript/box_mall.js
+++ sb/javascript/box_mall.js
@@ -1,1 +1,1 @@
-hash_box_mall={179:{id:179,name:3351,MB_price:4,launch:1,shelves:2018011811,limit:1,currency_1:1,items:179,str_id:2351,pic:"ads_351.jpg"}};
+hash_box_mall={180:{id:180,name:3354,MB_price:4,launch:1,shelves:2018012511,limit:1,currency_1:1,items:180,str_id:2354,pic:"ads_354.jpg"}};
